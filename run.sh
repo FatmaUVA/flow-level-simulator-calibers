b62b40936178f19aab1eacef11cc52c84eb046aa
fha6np <fha6np@node-3.fgcs-paper-2.ch-geni-net.uky.emulab.net>
2018-06-07 15:45:44 -0400

    change run.sh to execute 2 runs

--- run.sh
+++ run.sh
@@ -1,16 +1,24 @@
 network='G-scale' #linear one-link or esnet
 avg_rate=100 #$2
 ver=1
+count=0
 for run_no in `seq 1 10`
 do
-for sched in  'new-local' 'new-global' 'new-fixed'
+for sched in  'new-fixed' #'new-global' 'new-fixed'
 do
-    for algo in 'sjf' 'ljf' 
+    for algo in 'sjf' #'ljf' 
     do
     	if [ $sched = 'new-fixed' ] && [ $algo = 'ljf' ]; then
             continue
         else
-            python run-simulation.py $network $sched $algo $ver $avg_rate $run_no
+	    if [ $((count%2)) -eq 0 ]; then
+                echo started $network $sched $algo $ver $avg_rate $run_no
+                python run-simulation.py $network $sched $algo $ver $avg_rate $run_no &
+            else
+                echo started $network $sched $algo $ver $avg_rate $run_no
+                python run-simulation.py $network $sched $algo $ver $avg_rate $run_no
+            fi
+            count=$((count+1))
         fi
     done
 done
